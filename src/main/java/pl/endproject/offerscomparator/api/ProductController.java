ddfcd91ecc5d36e67c2b130ab6ece97da2d44780
Tomasz Tworek <tomasztworek27@gmail.com>
2020-06-02 19:28:48 +0200

    @04 Frontend - add list clear to apiAllegro

--- src/main/java/pl/endproject/offerscomparator/api/ProductController.java
+++ src/main/java/pl/endproject/offerscomparator/api/ProductController.java
@@ -35,7 +35,6 @@ public class ProductController {
 
     @PostMapping("/findProducts")
     public String findOffers(@RequestParam String userSearch) {
-
         return "redirect:/offers?userSearch=" + userSearch;
     }
 
